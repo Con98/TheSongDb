6979bf0f67f1a8f284066c88e07ae0080f5a64b8
Emmet <emmetmceneaney@gmail.com>
2018-02-22 13:40:49 +0000

    Friends JSP page

--- TheSongDbSite/src/main/java/Daos/FriendDao.java
+++ TheSongDbSite/src/main/java/Daos/FriendDao.java
@@ -80,7 +80,7 @@ public class FriendDao extends Dao implements FriendDaoInterface {
         }
         return friends;
     }
-    
+
     /**
      *
      * @param username1
@@ -88,47 +88,39 @@ public class FriendDao extends Dao implements FriendDaoInterface {
      * @return
      */
     @Override
-    public int addFriendship(String username1, String username2){
+    public int addFriendship(String username1, String username2) {
         Connection con = null;
         PreparedStatement ps = null;
         int rowsAffected = 0;
-        
-        try{
+
+        try {
             con = this.getConnection();
-            
+
             String query = "INSERT INTO friend(friend1, friend2) VALUES (?, ?)";
             ps = con.prepareStatement(query);
             ps.setString(1, username1);
             ps.setString(2, username2);
-            
+
             rowsAffected = ps.executeUpdate();
-        }
-        catch (SQLException ex){
+        } catch (SQLException ex) {
             System.err.println("\tA problem occurred during the addFriendship method:");
-            System.err.println("\t"+ex.getMessage());
+            System.err.println("\t" + ex.getMessage());
             rowsAffected = 0;
-        } 
-        finally 
-        {
-            try 
-            {
-                if (ps != null) 
-                {
+        } finally {
+            try {
+                if (ps != null) {
                     ps.close();
                 }
-                if (con != null) 
-                {
+                if (con != null) {
                     freeConnection(con);
                 }
-            } 
-            catch (SQLException e) 
-            {
+            } catch (SQLException e) {
                 System.err.println("A problem occurred when closing down the addFriendship method:\n" + e.getMessage());
             }
         }
         return rowsAffected;
     }
-    
+
     /**
      *
      * @param username1
@@ -136,100 +128,86 @@ public class FriendDao extends Dao implements FriendDaoInterface {
      * @return
      */
     @Override
-    public boolean removeFriendship(String username1, String username2){
+    public boolean removeFriendship(String username1, String username2) {
         Connection con = null;
         PreparedStatement ps = null;
         boolean removed = false;
-        
-        try{
+
+        try {
             con = this.getConnection();
-            
+
             String query = "DELETE FROM friend WHERE (friend1 = ? AND friend2 = ?) OR (friend1 = ? AND friend2 = ?)";
             ps = con.prepareStatement(query);
             ps.setString(1, username1);
             ps.setString(2, username2);
             ps.setString(3, username2);
             ps.setString(4, username1);
-            
+
             int rowsAffected = ps.executeUpdate();
-            if (rowsAffected != 0){
+            if (rowsAffected != 0) {
                 removed = true;
             }
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.err.println("\tA problem occurred during the removeFriendship method:");
-            System.err.println("\t"+ex.getMessage());
+            System.err.println("\t" + ex.getMessage());
             removed = false;
-        }
-        
-        finally 
-        {
-            try 
-            {
-                if (ps != null) 
-                {
+        } finally {
+            try {
+                if (ps != null) {
                     ps.close();
                 }
-                if (con != null) 
-                {
+                if (con != null) {
                     freeConnection(con);
                 }
-            } 
-            catch (SQLException ex) 
-            {
+            } catch (SQLException ex) {
                 System.err.println("A problem occurred during closing down the removeFriendship method:\n" + ex.getMessage());
             }
         }
         return removed;
     }
-    
+
     /**
      *
      * @param username1
      * @return
      */
     @Override
-    public boolean removeUserFriends(String username1){
+    public boolean removeUserFriends(String username1) {
         Connection con = null;
         PreparedStatement ps = null;
         boolean removed = false;
-        
-        try{
+
+        try {
             con = this.getConnection();
-            
+
             String query = "DELETE FROM friend WHERE friend1 = ? OR friend2 = ?";
             ps = con.prepareStatement(query);
             ps.setString(1, username1);
             ps.setString(2, username1);
-            
+
             int rowsAffected = ps.executeUpdate();
-            if(rowsAffected != 0){
+            if (rowsAffected != 0) {
                 removed = true;
             }
-        } catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println("\tA problem occurred during the removeUserFriends method:");
-            System.err.println("\t"+ex.getMessage());
+            System.err.println("\t" + ex.getMessage());
             removed = false;
-        }
-        finally{
-            try 
-            {
-                if (ps != null) 
-                {
+        } finally {
+            try {
+                if (ps != null) {
                     ps.close();
                 }
-                if (con != null) 
-                {
+                if (con != null) {
                     freeConnection(con);
                 }
-            } 
-            catch (SQLException ex) 
-            {
+            } catch (SQLException ex) {
                 System.err.println("A problem occurred in closing down the removeUserFriends method: \n" + ex.getMessage());
             }
         }
         return removed;
     }
-    
+
     /**
      *
      * @param username1
@@ -237,60 +215,51 @@ public class FriendDao extends Dao implements FriendDaoInterface {
      * @return
      */
     @Override
-    public Friend checkFriendshipStatus(String username1, String username2){
+    public Friend checkFriendshipStatus(String username1, String username2) {
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         Friend friends = null;
-        
-        try{
+
+        try {
             con = this.getConnection();
-            
+
             String query = "SELECT * FROM friend WHERE (friend1 = ? AND friend2 = ?) OR (friend1 = ? AND friend2 = ?)";
-            
+
             ps = con.prepareStatement(query);
             ps.setString(1, username1);
             ps.setString(2, username2);
             ps.setString(3, username2);
             ps.setString(4, username1);
-            
+
             rs = ps.executeQuery();
-            
-            if (rs.next()){
+
+            if (rs.next()) {
                 String uname1 = rs.getString("friend1");
                 String uname2 = rs.getString("friend2");
-                
+
                 UserDao userDao = new UserDao("MyFriends");
                 User friend1 = userDao.findUserByUsername(uname1);
                 User friend2 = userDao.findUserByUsername(uname2);
-                
+
                 friends = new Friend(friend1, friend2);
             }
-        }
-        catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println("\tA problem occurred during the checkFriendshipStatus method:");
-            System.err.println("\t"+ex.getMessage());
+            System.err.println("\t" + ex.getMessage());
             friends = null;
-        }
-        finally 
-        {
-            try 
-            {
-                if (rs != null) 
-                {
+        } finally {
+            try {
+                if (rs != null) {
                     rs.close();
                 }
-                if (ps != null) 
-                {
+                if (ps != null) {
                     ps.close();
                 }
-                if (con != null) 
-                {
+                if (con != null) {
                     freeConnection(con);
                 }
-            } 
-            catch (SQLException ex) 
-            {
+            } catch (SQLException ex) {
                 System.err.println("A problem occurred when closing down the checkFriendshipStatus method:\n" + ex.getMessage());
             }
         }
---
+++ TheSongDbSite/src/main/webapp/friends.jsp
@@ -0,0 +1,45 @@
+<%-- 
+    Document   : friends
+    Created on : 22-Feb-2018, 13:29:48
+    Author     : emmet
+--%>
+<%@page import="Daos.FriendDao"%>
+<%@page import="Daos.UserDao" %>
+<%@page import="Dtos.User" %>
+<%@page import="Dtos.Friend" %>
+<%@page import="java.util.ArrayList"%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Friends List</title>
+    </head>
+    <body>
+        <%
+            UserDao userDao = new UserDao("myFriends");
+            Object user = session.getAttribute("userName");
+            String username = (String) user;
+            if (username != null || !username.equals("")) {
+                FriendDao friendDao = new FriendDao("myFriends");
+                ArrayList<Friend> friends = friendDao.displayAllFriends(username);
+
+        %>
+        <h1><%=username%>'s Friend List</h1>
+        <table>
+            <%
+                for (Friend f : friends) {
+            %>
+            <tr>
+                <td><%=f.getFriend2()%></td>
+                <%
+                        }
+                    } else {
+                        out.println("No Friends Found");
+                    }
+                %>
+            </tr>
+        </table>
+    </body>
+</html>
